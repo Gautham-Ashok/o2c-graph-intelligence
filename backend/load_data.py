fa1125935ea3ed3eff4bab4e7c77649eb9cc3272
Motz007 <gauthamashok18@gmail.com>
2026-03-25 20:22:36 +0530

    Fix DB connection for Railway

--- backend/load_data.py
+++ backend/load_data.py
@@ -6,10 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 conn = psycopg2.connect(
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    host=os.getenv("DB_HOST")
+    os.getenv("DATABASE_URL"),
+    sslmode="require"
 )
 cursor = conn.cursor()
 
